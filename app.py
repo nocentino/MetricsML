0ec8a6b448c89c4fa68feb45bab45fa4cea9850a
Anthony E. Nocentino <nocentino@users.noreply.github.com>
2024-03-30 09:55:29 -0500

    added multiple server support

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from prophet import Prophet
 from prometheus_client import start_http_server, Gauge, make_wsgi_app
 from wsgiref.simple_server import make_server
 
+
 # Class to hold the sql instance name and the dictionary of metrics
 class sql_instance:
     def __init__(self, sql_instance_name):
@@ -12,9 +13,9 @@ class sql_instance:
         self.metrics = {}
         
 
-def get_predictions():
+def get_prometheus_data():
     # Prometheus api endpoint for query
-    URL = os.getenv('PROMETHEUS') 
+    URL = 'http://localhost:9090/api/v1/query'
 
 
     # CPU Query
@@ -29,62 +30,105 @@ def get_predictions():
     r1_json = r1.json()
 
 
-    # A dataframe that converts the json to a dataframe ready for prophet with timestamp as the first columm and the metric value as the second column for each result
-    df = pandas.DataFrame()  
-
-    # for each result in the json response, create a dataframe with the timestamp and the metric value
+    # for each result in the response create a data structure to hold the sql_instance name and a data frame of the returned data and add it to the list of data frames
+    dataframes = []
     for result in r1_json['data']['result']:
-        df = pandas.concat([df, pandas.DataFrame(result['values'], columns=['ds', 'y'])])
-        
-
-    # Use prophet to predict a value 5 minutes in the future based off of the data in the data frame
-    df['ds'] = pandas.to_datetime(df['ds'], unit='s')
-    m = Prophet()
-    m.fit(df)
-    future = m.make_future_dataframe(periods=300, freq='s')      #Automatically fits to sampling interval in the data set, here its 30 seconds
-    forecast = m.predict(future)                                 #Will predict each interval up until the number of periods
-
-    # Extract the metric name, hostname from the json response. Build the string that is the new metric name.
-    my_predicted_sql_instance = r1_json['data']['result'][0]['metric'].get('sql_instance')
-    predicted_metric_name = r1_json['data']['result'][0]['metric'].get('__name__')
-    predicted_metric_name_yhat = predicted_metric_name + '_yhat'
-    predicted_metric_name_yhat_lower = predicted_metric_name + '_yhat_lower'
-    predicted_metric_name_yhat_upper = predicted_metric_name + '_yhat_upper'
+        my_sql_instance = result['metric'].get('sql_instance')
+        my_metric_name = result['metric'].get('__name__')
+        my_dataframe = pandas.DataFrame(result['values'], columns=['ds', 'y'])
+        dataframes.append({'sql_instance': my_sql_instance, 'metric_name': my_metric_name, 'dataframe': my_dataframe})
 
 
-    # Load up the predicted value and the lower and upper bounds, only non-negative values are allowed
-    predicted_metric_value_yhat  = max ( 0, forecast[['yhat']].tail(1).values[0][0] )
-    predicted_metric_value_lower = max ( 0, forecast[['yhat_lower']].tail(1).values[0][0] )
-    predicted_metric_value_upper = max ( 0, forecast[['yhat_upper']].tail(1).values[0][0] )
+    return dataframes
 
 
-    # Create a dictonary for the predicted metric values and the lower and upper bounds
-    predicted_metrics = {
-        predicted_metric_name_yhat: predicted_metric_value_yhat, 
-        predicted_metric_name_yhat_lower: predicted_metric_value_lower, 
-        predicted_metric_name_yhat_upper: predicted_metric_value_upper}
-
-    # instantiate the sql_instance class
-    my_sql_instance = sql_instance(my_predicted_sql_instance)
-    my_sql_instance.metrics = predicted_metrics
+def get_predictions():
+    # Get the data from get_prometheus_data
+    dataframes = get_prometheus_data()
     
-    print("Get metrics...\nPrometheus Results Evaluated: " + str(df.y.count()) )
 
-    return my_sql_instance
+    # Array to hold the sql_instance objects
+    my_sql_instances = []
+    
+    
+    # For each data frame in the dataframes list get the data and predict the next value
+    for data in dataframes:
+        df = data['dataframe']
+        my_predicted_sql_instance = data['sql_instance']
+        predicted_metric_name = data['metric_name']
+        print("Predicting for: " + my_predicted_sql_instance + "\tMetric: " + predicted_metric_name)
+    
+    
+        # Use prophet to predict a value 5 minutes in the future based off of the data in the data frame
+        df['ds'] = pandas.to_datetime(df['ds'], unit='s')
+        m = Prophet()
+        m.fit(df)
+        future = m.make_future_dataframe(periods=300, freq='s')      #Automatically fits to sampling interval in the data set, here its 30 seconds
+        forecast = m.predict(future)                                 #Will predict each interval up until the number of periods
+    
+    
+        # Extract the metric name, hostname from the json response. Build the string that is the new metric name.
+        predicted_metric_name_yhat = predicted_metric_name + '_yhat'
+        predicted_metric_name_yhat_lower = predicted_metric_name + '_yhat_lower'
+        predicted_metric_name_yhat_upper = predicted_metric_name + '_yhat_upper'
+    
+    
+        # Load up the predicted value and the lower and upper bounds, only non-negative values are allowed
+        predicted_metric_value_yhat  = max ( 0, forecast[['yhat']].tail(1).values[0][0] )
+        predicted_metric_value_lower = max ( 0, forecast[['yhat_lower']].tail(1).values[0][0] )
+        predicted_metric_value_upper = max ( 0, forecast[['yhat_upper']].tail(1).values[0][0] )
+    
+    
+        # Create a dictonary for the predicted metric values and the lower and upper bounds
+        predicted_metrics = {
+            predicted_metric_name_yhat: predicted_metric_value_yhat, 
+            predicted_metric_name_yhat_lower: predicted_metric_value_lower, 
+            predicted_metric_name_yhat_upper: predicted_metric_value_upper}
+    
+    
+        # instantiate the sql_instance class
+        my_sql_instance = sql_instance(my_predicted_sql_instance)
+        my_sql_instance.metrics = predicted_metrics
+    
+    
+        # Append the sql_instance object to the my_sql_instances list
+        my_sql_instances.append(my_sql_instance)
+    
+    
+        # print the predicted values to the console
+        print("Prometheus Results Evaluated: " + str(df.y.count()) + "\tPredicted Value: " + str(predicted_metric_value_yhat) + "\tLower Bound: " + str(predicted_metric_value_lower) + "\tUpper Bound: " + str(predicted_metric_value_upper))
+
+
+    # print all of the sql_instance objects to the console
+    for instance in my_sql_instances:
+        print("SQL Instance: " + instance.sql_instance_name)
+        for metricname, metricvalue in instance.metrics.items():
+            print("\tMetric: " + metricname + "\tValue: " + str(metricvalue))
+            
+    return my_sql_instances
+
 
 def get_metrics():
     predicted_metrics = get_predictions()
     
-    # if this is the first page load create the gauge metrics and set the labels
+    # if this is the first time the page create a gauge metric for the first instance in the predicted_metrics list
     if not hasattr(get_metrics, 'gauge'):
         get_metrics.gauge = {}
-        for metricname, metricvalue in predicted_metrics.metrics.items():
-            get_metrics.gauge[metricname] = Gauge(metricname, metricname, ['sql_instance'])
-            
-            
+        
+        # create a gauge metric for each metric in the first instance in the predicted_metrics list
+        instance = predicted_metrics[0]
+        for metricname, metricvalue in instance.metrics.items():
+            get_metrics.gauge[metricname] = Gauge(metricname, 'Predicted Metric : ' + metricname, ['sql_instance'])
+
+        for instance in predicted_metrics:
+            for metricname, metricvalue in instance.metrics.items():
+                get_metrics.gauge[metricname].labels(sql_instance=instance.sql_instance_name).set(metricvalue)
+  
     # on subsequent page loads update the gauge metrics
-    for metricname, metricvalue in predicted_metrics.metrics.items():
-            get_metrics.gauge[metricname].labels(sql_instance=predicted_metrics.sql_instance_name).set(metricvalue)
+    for instance in predicted_metrics:
+        for metricname, metricvalue in instance.metrics.items():
+            get_metrics.gauge[metricname].labels(sql_instance=instance.sql_instance_name).set(metricvalue)
+
 
 
 def my_app(environ, start_fn):
@@ -96,5 +140,5 @@ def my_app(environ, start_fn):
 
 if __name__ == '__main__':
     metrics_app = make_wsgi_app()
-    httpd = make_server('', 8000, my_app)
+    httpd = make_server('', 8001, my_app)
     httpd.serve_forever()
